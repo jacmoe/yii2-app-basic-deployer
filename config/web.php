32e9b98dd947c3c67a42157df8766fab35d6756c
Jacob Moen <jacmoe.dk@gmail.com>
2016-01-21 20:28:37 +0100

    Removing the cookie key from config/web.php since the project is meant to be generated by Composer first.

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'PaldaMQ2zFEsAhv2SPRlp2it_XNnSuJz',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
