993dd8d3316c74e05efc23159520e3020eed7a9c
Jacob Moen <jacmoe.dk@gmail.com>
2016-01-22 15:05:45 +0100

    Logic error in yii-configure fixed

--- deployer/recipe/yii-configure.php
+++ deployer/recipe/yii-configure.php
@@ -47,14 +47,11 @@ task('deploy:configure', function () {
 
     $tmpDir = sys_get_temp_dir();
     $deployDir = env('deploy_path');
+    $releaseDir = env('release_path');
 
     /* @var $file \Symfony\Component\Finder\SplFileInfo */
     foreach ($iterator as $file) {
         $success = false;
-        $yii_file = false;
-        if(basename($file) === 'yii.tpl') {
-            $yii_file = true;
-        }
         // Make tmp file
         $tmpFile = tempnam($tmpDir, 'tmp');
         if (!empty($tmpFile)) {
@@ -63,10 +60,9 @@ task('deploy:configure', function () {
                 $target   = preg_replace('/\.tpl$/', '', $file->getRelativePathname());
                 // Put contents and upload tmp file to server
                 if (file_put_contents($tmpFile, $contents) > 0) {
-                    if($yii_file) {
-                        $deployDir = env('release_path');
-                        upload($tmpFile, "$deployDir/" . $target);
-                        run('chmod +x ' . "$deployDir/" . $target);
+                    if(basename($file) === 'yii.tpl') {
+                        upload($tmpFile, "$releaseDir/" . $target);
+                        run('chmod +x ' . "$releaseDir/" . $target);
                     } else {
                         run("mkdir -p $deployDir/shared/" . dirname($target));
                         upload($tmpFile, "$deployDir/shared/" . $target);
