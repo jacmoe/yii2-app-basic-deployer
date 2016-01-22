235124184078f5528470445a67986f2b8afafc63
Jacob Moen <jacmoe.dk@gmail.com>
2016-01-22 15:08:18 +0100

    Cookie key for testing

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'PaldaMQ2zFEsAhv2SPRlp2it_XNnSuJz',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
